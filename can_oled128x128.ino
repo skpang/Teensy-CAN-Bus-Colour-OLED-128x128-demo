484b04806e629068c6928f1b4c649fd13038759c
Sukkin Pang <skp@skpang.co.uk>
2019-03-11 11:37:34 +0000

    Update can_oled128x128.ino

--- can_oled128x128.ino
+++ can_oled128x128.ino
@@ -5,44 +5,16 @@ V1.0 March 2017
 For use with Teensy CAN-Bus with Colour OLED 128x128 and SD card holder:
 http://skpang.co.uk/catalog/teensy-canbus-with-colour-oled-128x128-and-sd-card-holder-p-1523.html
 
-Also requres new FlexCAN libarary
-https://github.com/collin80/FlexCAN_Library
+Also requres new Teensy SSD1351 libarary
+https://github.com/kirberich/teensy_ssd1351
 
 */
 
-#define sclk 2
-#define mosi 3
-#define dc   15
-#define cs   10
-#define rst  14
-
-// Color definitions
-#define	BLACK           0x0000
-#define	BLUE            0x001F
-#define	RED             0xF800
-#define	GREEN           0x07E0
-#define CYAN            0x07FF
-#define MAGENTA         0xF81F
-#define YELLOW          0xFFE0  
-#define WHITE           0xFFFF
-#include <FlexCAN.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1351.h>
-#include <Fonts/FreeMono9pt7b.h>
-#include <Fonts/FreeSans9pt7b.h>
-#include <Fonts/FreeSans12pt7b.h>
-#include <Fonts/FreeSansBold12pt7b.h>
-#include <Fonts/FreeSansBold18pt7b.h>
+#include <Arduino.h>
 #include <SPI.h>
+#include <ssd1351.h>
+#include <FlexCAN.h>
 
-// Option 1: use any pins but a little slower
-//Adafruit_SSD1351 tft = Adafruit_SSD1351(cs, dc, mosi, sclk, rst);  
-
-// Option 2: must use the hardware SPI pins 
-// (for UNO thats sclk = 13 and sid = 11) and pin 10 must be 
-// an output. This is much faster - also required if you want
-// to use the microSD card (see the image drawing example)
-Adafruit_SSD1351 tft = Adafruit_SSD1351(cs, dc, rst);
 
 static CAN_message_t msg,rxmsg;
 volatile uint32_t count = 0;
@@ -50,37 +22,40 @@ IntervalTimer TX_timer;
 String CANStr(""); 
 volatile uint32_t can_msg_count = 0;
 
+// use this to do Color c = RGB(...) instead of `RGB c = RGB(...)` or ssd1351::LowColor c = RGB(...)
+// because it's slightly faster and guarantees you won't be sending wrong colours to the display.
+
+// Choose color depth - IndexedColor, LowColor and HighColor currently supported
+// typedef ssd1351::IndexedColor Color;
+// typedef ssd1351::LowColor Color;
+typedef ssd1351::HighColor Color;
+
+// Choose display buffering - NoBuffer or SingleBuffer currently supported
+// auto display = ssd1351::SSD1351<Color, ssd1351::NoBuffer, 128, 96>();
+auto display = ssd1351::SSD1351<Color, ssd1351::SingleBuffer, 128, 128>();
+
 
 void setup(void) {
   Serial.begin(9600);
-  Serial.print("hello!");
-  tft.begin();
-
-  Serial.println("init");
-
-  // You can optionally rotate the display by running the line below.
-  // Note that a value of 0 means no rotation, 1 means 90 clockwise,
-  // 2 means 180 degrees clockwise, and 3 means 270 degrees clockwise.
-  //tft.setRotation(1);
-  // NOTE: The test pattern at the start will NOT be rotated!  The code
-  // for rendering the test pattern talks directly to the display and
-  // ignores any rotation.
-
-
+  Serial.print("CAN-Bus demo");
   
-tft.setCursor(0,0);
-  tft.fillScreen(BLACK);
- tft.print("Hello");
+  display.begin();
+  display.fillScreen(ssd1351::RGB());
 
-tft.setCursor(0,20);
-// tft.setFont(&FreeSansBold12pt7b);
-  tft.print("Testing ");
-  delay(500);
-//tftPrintTest();
-  Serial.println("done");
-  delay(100);
-    Can0.begin(500000); //set speed here. 
-      msg.buf[0] = 1;
+  display.setFont(FreeSans9pt7b);
+//  display.setTextSize(3);
+
+  display.setTextColor(ssd1351::RGB(255, 255, 255));
+  
+  display.setCursor(0,20);
+  display.println("128x128 OLED");
+  display.println("CAN-Bus demo ");
+  display.println("SK Pang");
+  display.updateScreen();
+
+
+  Can0.begin(500000); //set speed here. 
+  msg.buf[0] = 1;
   msg.buf[1] = 2;
   msg.buf[2] = 0;
   msg.buf[3] = 0;
@@ -89,7 +64,7 @@ tft.setCursor(0,20);
   msg.buf[6] = 0;
   msg.len = 8;
   msg.id = 0x222;
-  msg.timeout = 500;
+
   TX_timer.begin(tx_CAN, 1000000);    /* Start interrutp timer */
 
     
@@ -124,12 +99,12 @@ void loop()
      Serial.print(' ');
      Serial.println(CANStr);  
      
-     tft.fillRect(0,15,128, 30,BLACK);
-     tft.setCursor(0,15);
+   //  tft.fillRect(0,15,128, 30,BLACK);
+   //  tft.setCursor(0,15);
      
-     tft.println(rxmsg.id,HEX); 
-     tft.println(rxmsg.len,HEX); 
-     tft.println(CANStr);
+   //  tft.println(rxmsg.id,HEX); 
+   //  tft.println(rxmsg.len,HEX); 
+   //  tft.println(CANStr);
    //  tft.display();
 
      
@@ -139,117 +114,3 @@ void loop()
 
 
 }
-
-
-
-void testdrawtext(char *text, uint16_t color) {
-  tft.setCursor(0,0);
-  tft.setTextColor(color);
-  tft.print(text);
-}
-
-
-void testdrawrects(uint16_t color) {
- tft.fillScreen(BLACK);
- for (uint16_t x=0; x < tft.height()-1; x+=6) {
-   tft.drawRect((tft.width()-1)/2 -x/2, (tft.height()-1)/2 -x/2 , x, x, color);
- }
-}
-
-void testfillrects(uint16_t color1, uint16_t color2) {
- tft.fillScreen(BLACK);
- for (uint16_t x=tft.height()-1; x > 6; x-=6) {
-   tft.fillRect((tft.width()-1)/2 -x/2, (tft.height()-1)/2 -x/2 , x, x, color1);
-   tft.drawRect((tft.width()-1)/2 -x/2, (tft.height()-1)/2 -x/2 , x, x, color2);
- }
-}
-
-void testfillcircles(uint8_t radius, uint16_t color) {
-  for (uint8_t x=radius; x < tft.width()-1; x+=radius*2) {
-    for (uint8_t y=radius; y < tft.height()-1; y+=radius*2) {
-      tft.fillCircle(x, y, radius, color);
-    }
-  }  
-}
-
-void testdrawcircles(uint8_t radius, uint16_t color) {
-  for (uint8_t x=0; x < tft.width()-1+radius; x+=radius*2) {
-    for (uint8_t y=0; y < tft.height()-1+radius; y+=radius*2) {
-      tft.drawCircle(x, y, radius, color);
-    }
-  }  
-}
-
-void testtriangles() {
-  tft.fillScreen(BLACK);
-  int color = 0xF800;
-  int t;
-  int w = tft.width()/2;
-  int x = tft.height();
-  int y = 0;
-  int z = tft.width();
-  for(t = 0 ; t <= 15; t+=1) {
-    tft.drawTriangle(w, y, y, x, z, x, color);
-    x-=4;
-    y+=4;
-    z-=4;
-    color+=100;
-  }
-}
-
-void testroundrects() {
-  tft.fillScreen(BLACK);
-  int color = 100;
-  
-  int x = 0;
-  int y = 0;
-  int w = tft.width();
-  int h = tft.height();
-  for(int i = 0 ; i <= 24; i++) {
-    tft.drawRoundRect(x, y, w, h, 5, color);
-    x+=2;
-    y+=3;
-    w-=4;
-    h-=6;
-    color+=1100;
-    Serial.println(i);
-  }
-}
-
-void tftPrintTest() {
-  tft.fillScreen(BLACK);
-  tft.setCursor(0, 5);
-  tft.setTextColor(RED);  
-  tft.setTextSize(1);
-  tft.println("Hello World!");
-  tft.setTextColor(YELLOW);
-  tft.setTextSize(2);
-  tft.println("Hello World!");
-  tft.setTextColor(BLUE);
-  tft.setTextSize(3);
-  tft.print(1234.567);
-  delay(1500);
-  tft.setCursor(0, 5);
-  tft.fillScreen(BLACK);
-  tft.setTextColor(WHITE);
-  tft.setTextSize(0);
-  tft.println("Hello World!");
-  tft.setTextSize(1);
-  tft.setTextColor(GREEN);
-//  tft.print(p, 6);
-  tft.println(" Want pi?");
-  tft.println(" ");
-  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
-  tft.println(" Print HEX!");
-  tft.println(" ");
-  tft.setTextColor(WHITE);
-  tft.println("Sketch has been");
-  tft.println("running for: ");
-  tft.setTextColor(MAGENTA);
-  tft.print(millis() / 1000);
-  tft.setTextColor(WHITE);
-  tft.print(" seconds.");
-}
-
-
-
